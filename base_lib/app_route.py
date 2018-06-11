cc9c2660dfb39e6947eb6c8dfff6c0e921717381
bozyao <13691161342@139.com>
2018-06-11 14:37:18 +0800

    Add current_user in handler with session

--- base_lib/app_route.py
+++ base_lib/app_route.py
@@ -209,12 +209,13 @@ def check_session(permission="login"):
 
     def fc(func):
         def _(self, *args, **argitems):
-            if not self.session or not self.session.get("user_id"):
+            if not self.session or not self.session.get("user_id", 0):
                 self.ret_error("SESSIONERR", "需要登录后才可以操作哦")
             else:
                 self.user_id = self.session.get("user_id", 0)
+                self.current_user = self.session.get("user_id", 0)
                 self.open_id = self.session.get("open_id", "")
-                logging.info("User session checked by user_id: %s" % self.session.get("user_id", ""))
+                logging.info("User session checked by user_id: %s" % self.session.get("user_id", 0))
                 return func(self, *args, **argitems)
 
         return _
